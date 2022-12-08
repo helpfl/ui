2074e546a23cedd21ac36d9b37571af90df21208
Stephen McGuckin <stephen.mcguckin@nordicsemi.no>
2022-12-08 13:22:39 -0800

    adding infra stack for the spa

--- infrastructure/app-stack.ts
+++ infrastructure/app-stack.ts
@@ -1,4 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
+import { Duration, RemovalPolicy } from 'aws-cdk-lib';
+import { DnsValidatedCertificate } from 'aws-cdk-lib/aws-certificatemanager';
+import { CachePolicy, Distribution } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
+import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
@@ -8,20 +14,48 @@ export class TestAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const bucket = new Bucket(this, 'HelpflSPABucket', {
+        publicReadAccess: true,
+        removalPolicy: RemovalPolicy.DESTROY,
+        websiteIndexDocument: 'index.html',
+        websiteErrorDocument: 'index.html'
+    });
 
+    const deployment = new BucketDeployment(this, "HelpflSPADeployment", {
+        sources: [
+            Source.asset('./dist/ui')
+        ],
+        destinationBucket: bucket
+    });
 
-    // cdk code for my web spa
-    new Bucket(this, 'Test Bucket', {});
+    const domainName = 'helpfl.click';
 
-    // const deployment = new BucketDeployment(this, 'DeployWebsite', {
-    //     sources: [Source.asset(path.join(__dirname, 'my-website'))],
-    //     destinationBucket: websiteBucket,
-    //   });
-      
-    //   new ConstructThatReadsFromTheBucket(this, 'Consumer', {
-    //     // Use 'deployment.deployedBucket' instead of 'websiteBucket' here
-    //     bucket: deployment.deployedBucket,
-    //   });
+    const hostedZone = HostedZone.fromLookup(this, 'HostedZone', { domainName });
+
+    const certificate = new DnsValidatedCertificate(this, 'HelpflCert', {
+        domainName,
+        hostedZone,
+        region: 'us-east-1'
+    });
+
+    const distribution = new Distribution(this, 'HelpflDistribution', {
+        defaultRootObject: 'index.html',
+        domainNames: [domainName],
+        certificate: certificate,
+        defaultBehavior: {
+            cachePolicy: new CachePolicy(this, 'HelpflCacheing', {
+                defaultTtl: Duration.minutes(1)
+            }),
+            origin: new S3Origin(bucket),
+        },
+    });
+
+    new ARecord(this, 'ARecord', {
+        zone: hostedZone,
+        target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+        ttl: Duration.minutes(1),
+    });
+
     
   }
 }
