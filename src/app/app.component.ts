0c458221311370d56445068bf41b282f736fd3fa
Stephen McGuckin <stephen.mcguckin@nordicsemi.no>
2022-12-08 17:02:23 -0800

    gets the scafoliding set up for the main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+const Mode = {
+  TERMINAL: 'terminal',
+  QR_CODE_GENERATOR: 'qr-code-generator',
+  NONE: 'none'
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ui';
+  title = 'helpfl';
+
+  private mode: typeof Mode[keyof typeof Mode] = Mode.NONE;
+
+  showTerminal() {
+    console.log('show terminal');
+    this.mode = Mode.TERMINAL;
+  }
+
+  showQrCodeGenerator() {
+    this.mode = Mode.QR_CODE_GENERATOR;
+  }
+
+  isTerminal() {
+    console.log('is terminal');
+    return this.mode === Mode.TERMINAL;
+  }
+
+  isQrCodeGenerator() {
+    return this.mode === Mode.QR_CODE_GENERATOR; 
+  }
+
 }
