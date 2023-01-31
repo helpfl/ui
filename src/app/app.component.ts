b9b8537c1da7109a9dd83f3a42484d73214bd044
Stephen McGuckin <stephen.mcguckin@nordicsemi.no>
2023-01-31 13:43:21 -0800

    blog ui stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,68 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import {marked} from 'marked';
+import { map, Observable, of, tap } from 'rxjs';
 
-const Mode = {
-  TERMINAL: 'terminal',
-  QR_CODE_GENERATOR: 'qr-code-generator',
-  NONE: 'none'
-};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'helpfl';
 
-  private mode: typeof Mode[keyof typeof Mode] = Mode.NONE;
+  constructor(private readonly httpClient: HttpClient) {}
 
-  showTerminal() {
-    console.log('show terminal');
-    this.mode = Mode.TERMINAL;
+
+  latestContent(): Observable<string> {
+    return this.allRawContent().pipe(map(([latest]) => this.render(latest)));
   }
 
-  showQrCodeGenerator() {
-    this.mode = Mode.QR_CODE_GENERATOR;
+  private allRawContent(): Observable<string[]> {
+    const cachedContent = this.cachedContent();
+    return cachedContent ?
+      of(cachedContent) : 
+      this.getMonthOfContent().pipe(
+        tap(this.cacheContent)
+      );
   }
 
-  isTerminal() {
-    console.log('is terminal');
-    return this.mode === Mode.TERMINAL;
+  private readonly getMonthOfContent = () => {
+    const now = Date.now();
+    const aMonthAgo = now - 1000 * 60 * 60 * 24 * 30;
+    const requestOptions = {
+      headers: {
+        start: now.toString(),
+        end: aMonthAgo.toString()
+      }
+    };
+    return this.httpClient.get<string[]>('https://api.helpfl.click/blog', requestOptions)
+  };
+
+  private readonly cacheContent = (content: string[]) => {
+    const item = {
+      time: Date.now(),
+      content
+    };
+    localStorage.setItem('content', JSON.stringify(item));
+  };
+
+  private readonly isLessThan1HourAgo = (time: number): boolean => {
+    const anHourAgo = Date.now() - 1000 * 60 * 60;
+    return time > anHourAgo;
   }
 
-  isQrCodeGenerator() {
-    return this.mode === Mode.QR_CODE_GENERATOR; 
-  }
+  private readonly cachedContent = (): string[] | void => {
+    const cachedItem = localStorage.getItem('content');
+    if (!cachedItem) {
+      return;
+    }
+    const { time, content } = JSON.parse(cachedItem);
+    if (this.isLessThan1HourAgo(time)) {
+      return content;
+    }
+  };
 
+  private render(content: string): string {
+    return marked.parse(content);
+  }
 }
