9d633865e7457fbd1fd72914e8057bf19b7ae8ba
Stephen McGuckin <stephen.mcguckin@nordicsemi.no>
2023-02-01 19:37:14 -0800

    adds ability to manage posts through a manifest file and static content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import {marked} from 'marked';
-import { catchError, EMPTY, map, Observable, of, tap } from 'rxjs';
+import { catchError, EMPTY, map, mergeMap, Observable, of, shareReplay, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,59 +10,29 @@ import { catchError, EMPTY, map, Observable, of, tap } from 'rxjs';
 })
 export class AppComponent {
 
-  constructor(private readonly httpClient: HttpClient) {}
-
-
-  latestContent(): Observable<string> {
-    return this.allRawContent().pipe(map(([latest]) => this.render(latest)));
+  constructor(private readonly httpClient: HttpClient) {
   }
 
-  private allRawContent(): Observable<string[]> {
-    const cachedContent = this.cachedContent();
-    return cachedContent ?
-      of(cachedContent) : 
-      this.getMonthOfContent().pipe(
-        tap(this.cacheContent)
-      );
-  }
-
-  private readonly getMonthOfContent = () => {
-    const now = Date.now();
-    const aMonthAgo = now - 1000 * 60 * 60 * 24 * 30;
-    const requestOptions = {
-      params: {
-        start: now.toString(),
-        end: aMonthAgo.toString()
-      }
-    };
-    return this.httpClient.get<string[]>('https://api.helpfl.click/blog', requestOptions);
+  private readonly getBlogManifest = (): Observable<Manifest> => {
+    return this.httpClient.get<Manifest>('/assets/manifest.json');
   };
 
-  private readonly cacheContent = (content: string[]) => {
-    const item = {
-      time: Date.now(),
-      content
-    };
-    localStorage.setItem('content', JSON.stringify(item));
-  };
-
-  private readonly isLessThan1HourAgo = (time: number): boolean => {
-    const anHourAgo = Date.now() - 1000 * 60 * 60;
-    return time > anHourAgo;
-  }
-
-  private readonly cachedContent = (): string[] | void => {
-    const cachedItem = localStorage.getItem('content1');
-    if (!cachedItem) {
-      return;
-    }
-    const { time, content } = JSON.parse(cachedItem);
-    if (this.isLessThan1HourAgo(time)) {
-      return content;
-    }
+  private readonly getLatestBlogPost = () => {
+    return this.getBlogManifest().pipe(
+      map(({files: [first]}) => first),
+      mergeMap((file) => this.httpClient.get(file, {responseType: 'text'})),
+    );
   };
 
   private render(content: string): string {
     return marked.parse(content);
   }
+
+  public readonly latestBlogPost$ = this.getLatestBlogPost().pipe(
+    shareReplay(1),
+    map(this.render),
+  );
+
 }
+
+type Manifest = {files: string[]};
