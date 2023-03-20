3e4f7fcba9498a7ce6b8c17f0aba9385697646f8
ipcampbell12 <ipcampbell12@gmail.com>
2023-03-20 10:59:28 -0700

    fixed app.css error

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import 'antd/dist/reset.css';
-import './App.css';
+// import './App.css';
 
 import App from './App';
 
@@ -9,43 +9,6 @@ test('display "Hello World" on initial render', () => {
         <App />
     )
 
-    expect(screen.getByRole('heading')).toHaveTextContent("Hello World")
+    expect(screen.getByRole('heading')).toHaveTextContent('Hello World')
 })
 
-
-// import { describe, afterAll, afterEach, beforeAll } from 'vitest';
-// import { setupServer } from 'msw/node';
-// import { rest } from 'msw';
-// describe('App', () => {
-
-//     const url = 'https://example.com';
-
-//     const posts = [
-//         {
-//             content: 'Hello World'
-//         }
-//     ];
-
-//     const restHandlers = [
-//         rest.get(url, (req, res, ctx) => {
-//             return res(ctx.status(200), ctx.json(posts));
-//         })
-//     ];
-
-//     const server = setupServer(...restHandlers);
-
-
-//     beforeAll(() => server.listen({onUnhandledRequest: 'error'}));
-
-//     afterAll(() => server.close());
-
-//     afterEach(() => server.resetHandlers());
-
-//     it('renders content', async () => {
-//         render(<App contentServiceUrl={url}/>);
-
-//         await waitFor(() => {
-//             expect(screen.getByText('Hello World')).toBeInTheDocument();
-//         });
-//     });
-// });
