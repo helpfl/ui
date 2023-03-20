27a2010bce1be0ec6993ac84aa8b7ae37d87f8ad
ipcampbell12 <ipcampbell12@gmail.com>
2023-03-20 11:31:49 -0700

    commented test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,17 @@
-// import { render, screen } from '@testing-library/react';
-// import 'antd/dist/reset.css';
-// import { BrowserRouter } from 'react-router-dom'
-// // import './App.css';
+import { render } from '@testing-library/react';
+import 'antd/dist/reset.css';
+import { BrowserRouter } from 'react-router-dom'
+// import './App.css';
 
-// import App from './App';
+import App from './App';
 
-// test('display "Hello World" on initial render', () => {
-//     render(
-//         <BrowserRouter>
-//             <App />
-//         </BrowserRouter>
-//     )
+test('display "Hello World" on initial render', () => {
+    render(
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    )
 
-//     expect(screen.getByRole('heading')).toHaveTextContent('Hello World')
-// })
+    // expect(screen.getByRole('heading')).toHaveTextContent('Hello World')
+})
 
