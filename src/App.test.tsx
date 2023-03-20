7e0672a8d0c380efc6bc18f500417d98db81d840
ipcampbell12 <ipcampbell12@gmail.com>
2023-03-20 11:17:22 -0700

    fixed test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import 'antd/dist/reset.css';
+import { BrowserRouter } from 'react-router-dom'
 // import './App.css';
 
 import App from './App';
 
 test('display "Hello World" on initial render', () => {
     render(
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     )
 
     expect(screen.getByRole('heading')).toHaveTextContent('Hello World')
