489870f658c651e4219e8e17e4a6601ab31be764
ipcampbell12 <ipcampbell12@gmail.com>
2023-03-27 16:09:19 -0700

    added tests for links

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import 'antd/dist/reset.css';
 import { BrowserRouter } from 'react-router-dom'
 // import './App.css';
+//import Nav from './components/Nav';
 
 import App from './App';
+
+
+
 
 test('app renders without error', () => {
     render(
@@ -15,3 +20,48 @@ test('app renders without error', () => {
     // expect(screen.getByRole('heading')).toHaveTextContent('Hello World')
 })
 
+
+
+test('get home page', () => {
+    render(<App />, { wrapper: BrowserRouter })
+
+    expect(screen.getByText('Home')).toBeInTheDocument();
+
+})
+
+const renderWithRouter = (ui: React.ReactElement, { route = '/' } = {}) => {
+    window.history.pushState({}, 'Test page', route)
+
+    return {
+        user: userEvent.setup(),
+        ...render(ui, { wrapper: BrowserRouter }),
+    }
+}
+
+
+test('get link1', async () => {
+    const { user } = renderWithRouter(<App />)
+
+    await user.click(screen.getByText(/Link 1/i))
+
+    expect(screen.getByText(/NOT FOUND/i)).toBeInTheDocument()
+
+})
+
+test('get link2', async () => {
+    const { user } = renderWithRouter(<App />)
+
+    await user.click(screen.getByText(/Link 2/i))
+
+    expect(screen.getByText(/NOT FOUND/i)).toBeInTheDocument()
+
+})
+
+test('get link3', async () => {
+    const { user } = renderWithRouter(<App />)
+
+    await user.click(screen.getByText(/Link 3/i))
+
+    expect(screen.getByText(/NOT FOUND/i)).toBeInTheDocument()
+
+})
