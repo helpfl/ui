7128a180e9d7e700030526d3888391b0818a89bb
ipcampbell12 <ipcampbell12@gmail.com>
2023-03-27 17:48:20 -0700

    moved cards to this component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import ContentCard from './ContentCard';
 
 const Home: React.FC = () => {
     return (
         <div>
-
+            <div>
+                {
+                    [...Array(5)].map((x, i) =>
+                        <ContentCard key={i} />
+                    )}
+            </div>
         </div>
     );
 }
